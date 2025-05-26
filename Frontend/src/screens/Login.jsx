5d848466443e7168c6d24d1212e5276c5c02780f
DiegoVaz <vazquezsotelodiego@gmail.com>
2025-05-26 14:24:28 -0600

    [ADD] Formulario de inicio de sesión

--- Frontend/src/screens/Login.jsx
+++ Frontend/src/screens/Login.jsx
@@ -1,10 +1,33 @@
 import { useState, useEffect } from "react";
-import { Prueba } from "../components/Prueba";
+import './css/login.css';
 
 export const Login = () => {
-    return(
-    <>
-        <Prueba />
-    </>
+    return (
+        <>
+            <div>
+                <div className="ring">
+                    <i style={{ "--clr": "#00ff0a" }}></i>
+                    <i style={{ "--clr": "#ff0057" }}></i>
+                    <i style={{ "--clr": "#fffd44" }}></i>
+                    <div className="login">
+                        <h2>Inicio de sesión</h2>
+                        <div className="inputBx">
+                            <input type="text" placeholder="Correo electrónico" />
+                        </div>
+                        <div className="inputBx">
+                            <input type="password" placeholder="Contraseña" />
+                        </div>
+                        <div className="inputBx">
+                            <input type="submit" value="Iniciar sesión" />
+                        </div>
+                        <div className="links">
+                            <a href="#">Olvidé mi contraseña</a>
+                            <a href="#">Registrarse</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </>
     );
 }
