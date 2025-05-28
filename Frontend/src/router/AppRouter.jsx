1cac940e1d14574d3ad01e33ee98370860d6a7e6
Isaac <jim.isaacmiranda@gmail.com>
2025-05-27 23:23:40 -0600

    feat: category form modal

--- Frontend/src/router/AppRouter.jsx
+++ Frontend/src/router/AppRouter.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import React from 'react';
 import { Login } from '../screens/Login';
+import CategoryForm from '../screens/categories/CategoryFormModal';
+import Test from '../screens/Test';
 
 const AppRouter = () => {
     return (
         <Router>
             <Routes>
                 <Route path='/' element={<Login />} />
-                
             </Routes>
         </Router>
     );
